d658f8714dcd3ba7d2f5ac20f43e6538a8068339
Kushagra-Gupta01 <kushagra.gupta0077@gmail.com>
2023-10-25 00:39:51 +0530

    main func and basic setup done

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/kushagra-gupta01/Proof-of-Work_BlockChain
+
+go 1.20
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
+
 import (
+	"encoding/json"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 const difficulty = 1
@@ -17,21 +28,58 @@ type Block struct{
 
 var BlockChain []Block
 
-func main(){
+var mutex sync.Mutex
 
+func main(){
+	err := godotenv.Load()
+	if err != nil{
+		log.Fatal(err)
+	}
+	
+	go func(){
+		t := time.Now()	
+		genesisBlock := Block{}
+		genesisBlock = Block{0,t.String(),0,calculateHash(genesisBlock),"",difficulty,""}
+		spew.Dump(genesisBlock)
+		mutex.Lock()
+		BlockChain = append(BlockChain, genesisBlock)
+		mutex.Unlock()
+	}()
+	log.Fatal(run())
 }
 
 func run() error{
+	mux := makeMuxRouter()
+	httpPort := os.Getenv("PORT")
+	log.Println("HTTP server is running and lsitening on port:",httpPort)
+	s := &http.Server{
+		Addr: ":"+httpPort,
+		Handler: mux,
+		ReadTimeout: 10*time.Second,
+		WriteTimeout: 10*time.Second,
+		MaxHeaderBytes: 1>>20,
+	}
 
+	if err := s.ListenAndServe();err != nil{
+		return err
+	}
+	return nil
 }
 
 func makeMuxRouter() http.Handler{
-	HandlerFunc("/", handleGetBlockchain).Methods("GET")
-	HandlerFunc("/", handleWriteBlock).Methods("POST")
+	muxRouter := mux.NewRouter()
+	muxRouter.HandlerFunc("/", handleGetBlockchain).Methods("GET")
+	muxRouter.HandlerFunc("/", handleWriteBlock).Methods("POST")
+	return muxRouter
 }
 
-func handleGetBlockchain(){
-
+func handleGetBlockchain(w http.ResponseWriter,h *http.Request){
+	bytes,err := json.MarshalIndent(BlockChain,""," ")
+	if err !=nil{
+		http.Error(w,err.Error(),http.StatusInternalServerError)
+		return
+	}
+	io.WriteString(w ,string(bytes))
 }
 
 func handleWriteBlock(){
